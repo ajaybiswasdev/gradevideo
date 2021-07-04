c6c4586330648a393733574e8aaf65806ccb7179
ajaybiswasdev <ajaybiswas.dev@gmail.com>
2021-07-04 10:20:35 +0530

    Updates

---
+++ precache-manifest.4d013b9837a90cede4da663acb570dfd.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c9dd7a17257d387c7bf5f407b7b091",
+    "url": "/gradevideo/index.html"
+  },
+  {
+    "revision": "629b305ff5ebdb548a66",
+    "url": "/gradevideo/static/css/2.8fe590c1.chunk.css"
+  },
+  {
+    "revision": "714a247733e314eec9e2",
+    "url": "/gradevideo/static/css/main.8d7f326e.chunk.css"
+  },
+  {
+    "revision": "629b305ff5ebdb548a66",
+    "url": "/gradevideo/static/js/2.6b1a8c23.chunk.js"
+  },
+  {
+    "revision": "45b4807b0885061d4b7e150805388911",
+    "url": "/gradevideo/static/js/2.6b1a8c23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9731a7bf4be6c73bd739",
+    "url": "/gradevideo/static/js/3.a9b6d27d.chunk.js"
+  },
+  {
+    "revision": "1790214efba34982a154466015e58e1d",
+    "url": "/gradevideo/static/js/3.a9b6d27d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "714a247733e314eec9e2",
+    "url": "/gradevideo/static/js/main.e61b26a8.chunk.js"
+  },
+  {
+    "revision": "affe156de08e1aa32c5f",
+    "url": "/gradevideo/static/js/runtime-main.0e3d27b2.js"
+  },
+  {
+    "revision": "6dd68039f5ad6cc18eef825a36636c7f",
+    "url": "/gradevideo/static/media/GradeTV-logo.6dd68039.png"
+  },
+  {
+    "revision": "d99f66b5e4fc82ba095ca93bc8413d7e",
+    "url": "/gradevideo/static/media/Maax-Bold.d99f66b5.ttf"
+  },
+  {
+    "revision": "9d8ad401313214b2c249137153e313b6",
+    "url": "/gradevideo/static/media/Maax-Medium.9d8ad401.ttf"
+  },
+  {
+    "revision": "1c39827874948c512fba8aefbd092afa",
+    "url": "/gradevideo/static/media/Maax.1c398278.ttf"
+  },
+  {
+    "revision": "dba0c688b8d5ee09a1e214aebd5d25e4",
+    "url": "/gradevideo/static/media/ProductSans-Bold.dba0c688.ttf"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/gradevideo/static/media/ProductSans.eae9c18c.ttf"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/gradevideo/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/gradevideo/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "df8c626474a73ab7a8b511655597c7c4",
+    "url": "/gradevideo/static/media/ProximaNova-Semibold.df8c6264.ttf"
+  },
+  {
+    "revision": "fa8483717767fec18b53af9f53e17c2c",
+    "url": "/gradevideo/static/media/add.fa848371.svg"
+  },
+  {
+    "revision": "66c8730188c056606b645d7ee8a45201",
+    "url": "/gradevideo/static/media/arrow-down.66c87301.svg"
+  },
+  {
+    "revision": "745d458550e673e08e97253933189802",
+    "url": "/gradevideo/static/media/arrow-up.745d4585.svg"
+  },
+  {
+    "revision": "fec6f408733ab797e69f014209f88f6c",
+    "url": "/gradevideo/static/media/arrow.fec6f408.svg"
+  },
+  {
+    "revision": "2cb9472fb82d91ea9ffc2b3cbd94e39e",
+    "url": "/gradevideo/static/media/ceo-photo.2cb9472f.jpg"
+  },
+  {
+    "revision": "f988d6ae332c444d601b6abc99a2d16d",
+    "url": "/gradevideo/static/media/core-bg.f988d6ae.png"
+  },
+  {
+    "revision": "6428b83e38e358d9395e69c7c3cade41",
+    "url": "/gradevideo/static/media/eBook1.6428b83e.jpg"
+  },
+  {
+    "revision": "23364c5d6ebd3d5f631d5b372f692dba",
+    "url": "/gradevideo/static/media/eBook2.23364c5d.png"
+  },
+  {
+    "revision": "b266683566db2674f5cb5637cc3a5a04",
+    "url": "/gradevideo/static/media/eBook3.b2666835.jpg"
+  },
+  {
+    "revision": "651e07cec8901cdc54d867cdbab28510",
+    "url": "/gradevideo/static/media/earth.651e07ce.svg"
+  },
+  {
+    "revision": "e69228f94b3f142d5f3202df599c9fc7",
+    "url": "/gradevideo/static/media/email.e69228f9.svg"
+  },
+  {
+    "revision": "1ef5f075d5d160a35d7e057b2afa64b4",
+    "url": "/gradevideo/static/media/hamburger.1ef5f075.svg"
+  },
+  {
+    "revision": "6d3eef1df2fdba04fd7582f6e104d597",
+    "url": "/gradevideo/static/media/howItWorks-bg.6d3eef1d.png"
+  },
+  {
+    "revision": "b00a5e67edf154e6acaf67ad30b83543",
+    "url": "/gradevideo/static/media/img1.b00a5e67.png"
+  },
+  {
+    "revision": "1ca69fc9bb4e1adebd009f0ae1ca6e36",
+    "url": "/gradevideo/static/media/img2.1ca69fc9.png"
+  },
+  {
+    "revision": "a2742c9456909337b33538d82694eb02",
+    "url": "/gradevideo/static/media/img3.a2742c94.png"
+  },
+  {
+    "revision": "013b7a0f7dd5f34842a312d9962f0126",
+    "url": "/gradevideo/static/media/key.013b7a0f.svg"
+  },
+  {
+    "revision": "ba13c12f06e67eb25192d64abd603a5f",
+    "url": "/gradevideo/static/media/math.ba13c12f.mp4"
+  },
+  {
+    "revision": "728a498795fb0871a7b60e4c86e94e1e",
+    "url": "/gradevideo/static/media/newClient.728a4987.mp4"
+  },
+  {
+    "revision": "f71f3083284f0fa94db1de96363f0e83",
+    "url": "/gradevideo/static/media/review-modal-mobile.f71f3083.png"
+  },
+  {
+    "revision": "b0ca23ce37906e161ec252254af51bcf",
+    "url": "/gradevideo/static/media/share-modal-web.b0ca23ce.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/gradevideo/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/gradevideo/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/gradevideo/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/gradevideo/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "e2bbc7eed5fa732ed1783583b2133c60",
+    "url": "/gradevideo/static/media/tick.e2bbc7ee.svg"
+  },
+  {
+    "revision": "566489e47be4459efa152578904585f1",
+    "url": "/gradevideo/static/media/trainVideo1.566489e4.mp4"
+  },
+  {
+    "revision": "1db7923349ba67ede67e539acf384656",
+    "url": "/gradevideo/static/media/trainVideo10.1db79233.mp4"
+  },
+  {
+    "revision": "dfdc35c1162f5f1c79b2d5d843c5582b",
+    "url": "/gradevideo/static/media/trainVideo11.dfdc35c1.mp4"
+  },
+  {
+    "revision": "076c742495b28dd8e898de4455b908f8",
+    "url": "/gradevideo/static/media/trainVideo2.076c7424.mp4"
+  },
+  {
+    "revision": "87891e35edce441b45096b56c7f80113",
+    "url": "/gradevideo/static/media/trainVideo3.87891e35.mp4"
+  },
+  {
+    "revision": "9d0c78c5843acf19952e1f3a4580ec4b",
+    "url": "/gradevideo/static/media/trainVideo4.9d0c78c5.mp4"
+  },
+  {
+    "revision": "8db5e52104ac3c41e2fd357778f68638",
+    "url": "/gradevideo/static/media/trainVideo5.8db5e521.mp4"
+  },
+  {
+    "revision": "ba4d25a5979ebbdd066fb5d4797c2957",
+    "url": "/gradevideo/static/media/trainVideo6.ba4d25a5.mp4"
+  },
+  {
+    "revision": "b30d470ad838fc25977895b4b0e8cfb6",
+    "url": "/gradevideo/static/media/trainVideo7.b30d470a.mp4"
+  },
+  {
+    "revision": "7f73b29f2e9b9a9d723ae2c659652bc6",
+    "url": "/gradevideo/static/media/trainVideo8.7f73b29f.mp4"
+  },
+  {
+    "revision": "a43023d76170027417f67d04f3dc0501",
+    "url": "/gradevideo/static/media/trainVideo9.a43023d7.mp4"
+  },
+  {
+    "revision": "a6cd62593ca4d45bed3c733c2f7b57eb",
+    "url": "/gradevideo/static/media/tutor-photo-3.a6cd6259.png"
+  }
+]);
